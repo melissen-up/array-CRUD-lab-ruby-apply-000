cb5e0274397a77ccb2d38c01a39b9aa9128c82e4
Melissa F <melissa.faulner@gmail.com>
2020-10-07 11:18:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = ["Han", "Leia", "Ben"]
+  array = ["Han", "Leia", "Ben", "Rey"]
 end
 
 def add_element_to_end_of_array(array, element)
